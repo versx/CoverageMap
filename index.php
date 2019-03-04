073e0f87ff7ff135b45c9950dbcd1af11d2b3d4d
versx <versefx@gmail.com>
2019-03-04 15:49:50 -0800

    Fix map extending to full page

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <script src="https://unpkg.com/leaflet@1.4.0/dist/leaflet.js" integrity="sha512-QVftwZFqvtRNi0ZyCtsznlKSWOStnDORoefr1enyq5mVL4tmKB3S/EnC3rRJcxCPavG10IcrVGSmPh6Qw5lwrg==" crossorigin=""></script>
   </head>
   <body>
-    <div id="mapid" style="height: 640px;width: 100%;"></div>
+    <div id="mapid" style="top: 0; left: 0; position: absolute; height: 100%; width: 100%;"></div>
   </body>
 </html>
 <script>
